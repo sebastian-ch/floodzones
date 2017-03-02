9451342d2b3dd62842b341ef23f07565cd00f7e9
Sebastian Hancock <sebastian.hancock@uky.edu>
2017-03-02 13:08:21 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -8,21 +8,20 @@
 
     });
 
-   /* map.setMaxBounds(map.getBounds(), {
-        padding: [50, 50]
-    }); */
+    /* map.setMaxBounds(map.getBounds(), {
+         padding: [50, 50]
+     }); */
 
     var tiles = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-   // var nfhl_url = "https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/28";
 
     var floods = L.esri.featureLayer({
         url: "https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/28",
         where: "SFHA_TF = 'T'",
-        fields: ['OBJECTID', 'SFHA_TF', 'FLD_ZONE',],
+        fields: ['OBJECTID', 'SFHA_TF', 'FLD_ZONE', ],
         simplifyFactor: 0.35,
         precision: 5,
         style: function (feature) {
@@ -46,15 +45,40 @@
             }
 
         }
-    }).addTo(map);
+    }); //.addTo(map);
+
+    addSearch();
 
 
+    function addSearch() {
 
-    var popupTemplate = "<h3>Flood Zone: {FLD_ZONE}</h3><br><h4>100 year? {SFHA_TF}";
+        var searchControl = L.esri.Geocoding.geosearch();
+        searchControl.onAdd = function (map) {
 
-    floods.bindPopup(function (e) {
-        return L.Util.template(popupTemplate, e.feature.properties)
-    });
+            var div = L.DomUtil.get("search");
+            
+            return div;
+        }
+
+        searchControl.addTo(map);
+        var results = L.layerGroup().addTo(map);
+        
+        searchControl.on("results", function(data){
+            
+            results.clearLayers();
+            for (var i = data.results.length - 1; i >= 0; i--) {
+                
+                results.addLayer(L.marker(data.results[i].latlng));
+            }
+        });
+
+    }
+
+    /* var popupTemplate = "<h3>Flood Zone: {FLD_ZONE}</h3><br><h4>100 year? {SFHA_TF}";
+
+     floods.bindPopup(function (e) {
+         return L.Util.template(popupTemplate, e.feature.properties)
+     }); */
 
 
     /*   function drawMap(zones) {
@@ -72,24 +96,24 @@
        } */
 
 
-    function drawLegend() {
+    /*   function drawLegend() {
 
-        var legend = L.control({
-            positon: 'bottomright'
-        });
+           var legend = L.control({
+               positon: 'bottomright'
+           });
 
-        legend.onAdd = function (map) {
+           legend.onAdd = function (map) {
 
-            var div = L.DomUtil.get("legend");
+               var div = L.DomUtil.get("legend");
 
-            L.DomEvent.disableScrollPropagation(div);
-            L.DomEvent.disableClickPropagation(div);
+               L.DomEvent.disableScrollPropagation(div);
+               L.DomEvent.disableClickPropagation(div);
 
-            return div;
-        }
+               return div;
+           }
 
-        legend.addTo(map);
+           legend.addTo(map);
 
-    }
+       } */
 
 })();
