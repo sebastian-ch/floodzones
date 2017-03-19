1d2e75a24c8c4dfec79bd2c53f0f979f9c2a5463
Sebastian Hancock <sebastian.hancock@uky.edu>
2017-03-18 22:29:24 -0400

    large updates

--- js/test.js
+++ js/test.js
@@ -3,35 +3,38 @@
     var map = L.map('map', {
         zoomSnap: .1,
         center: [37.5328, -77.4318],
-        zoom: 14
+        zoom: 15
 
     });
-
-    /*  var map = L.map('map', {
- L.mapbox.accessToken = 'pk.eyJ1Ijoic2ViYXN0aWFuLWNoIiwiYSI6ImNpejkxdzZ5YzAxa2gyd21udGpmaGU0dTgifQ.IrEd_tvrl6MuypVNUGU5SQ';
-    var map = L.mapbox.map('map', 'mapbox.streets').setView([37.6043, -77.3614], 14); */
-
+    var service = L.esri.featureLayerService({
+        url: 'https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/28'
+    });
     var searchAddress = "",
-        searchLatLng,
-        //bounds = L.latLngBounds(map.getBounds()),
-        bounds = map.getBounds(),
-        center = map.getCenter(),
-        boundsZoom = map.getBoundsZoom(bounds),
-        floodLayer,
-        floodLayerGroup = L.layerGroup(),
-        Qjson,
-        current_location,
-        current_accuracy;
+        searchLatLng, //inputAddress
+        currentLocation;
+    var floodLayerGroup = L.layerGroup(); //layergroup of flood maps
+    var baseControl = L.control({
+        position: 'topright'
+    });
 
+    baseControl.onAdd = function (map) {
+        var div = L.DomUtil.get("togGroup");
+        return div;
+    }
+    baseControl.addTo(map);
+
+    //jquery to grab the input text and query the map based
+    //on it. Also if "enter" is pressed, it will do the same
+    //as if you hit the "search" button
     $('.input').bind("enterKey", function (e) {
         searchAddress = $('.input').val();
-        //console.log(searchAddress);
+        $('.loading').show();
         geocodeAddress(searchAddress);
     });
 
     $('.btn').on('click', function (e) {
         searchAddress = $('.input').val();
-        //console.log(searchAddress);
+        $('.loading').show();
         geocodeAddress(searchAddress);
     })
 
@@ -41,35 +44,54 @@
         }
     });
 
-    var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    var service = L.esri.featureLayerService({
-        url: 'https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/28'
-    });
+    //try and locate the user
     map.locate({
         setView: true,
-        maxZoom: 14
-    })
+    });
+
+
+    //if you can locate the user, change the bounds, zoom
+    //run map query based off new bounds
     map.on('locationfound', onLocationFound);
+    //if you can't locate the user, just use the current bounds
     map.on('locationerror', onLocationError);
-    //queryFloodMap(bounds);
+
 
     function onLocationFound(e) {
-
-        current_location = L.marker(e.latlng).addTo(map);
+        currentLocation = L.marker(e.latlng).addTo(map);
         map.setZoom(14);
-        bounds = map.getBounds();
-        enter = map.getCenter();
-        queryFloodMap(bounds);
+        var bounds = map.getBounds();
+        var center = map.getCenter();
 
+        queryFloodMap(bounds);
     }
 
-    function onLocationError(e) {
+    function onLocationError() {
+        var bounds = map.getBounds();
+        var center = map.getCenter();
+    }
 
-        alert(e.message);
-        queryFloodMap(bounds);
+    //add basemap control
+    function baseMapControl() {
+        
+        var baseName = $('.toggle-group input').val();
+        var tileLayer = L.esri.basemapLayer(baseName).addTo(map);
+        var baseLabels = L.esri.basemapLayer(baseName + 'Labels').addTo(map);
+
+        $('.toggle-group input').change(function (e) {
+            
+            map.removeLayer(tileLayer);
+            map.removeLayer(baseLabels);
+
+            baseName = $(this).val();
+            console.log(baseName);
+
+            tileLayer = L.esri.basemapLayer(baseName);
+            baseLabels = L.esri.basemapLayer(baseName + 'Labels').addTo(map);
+
+            tileLayer.addTo(map);
+            baseLabels.addTo(map);
+        });
 
     }
 
@@ -78,11 +100,10 @@
         service.query()
             .within(bounds)
             .fields(['OBJECTID', 'DFIRM_ID', 'FLD_ZONE', 'SFHA_TF', 'SHAPE.AREA'])
-            .where("SHAPE.AREA > '.00000001'")
+            .where("SHAPE.AREA >= '.0000001'")
             .precision(4)
-            .simplify(map, 0.20)
+            .simplify(map, 0.30)
             .run(function (error, featureCollection, response) {
-
 
                 console.log(featureCollection);
 
@@ -97,7 +118,7 @@
 
     function makeMap(data) {
 
-        floodLayer = L.geoJson(data, {
+        var floodLayer = L.geoJson(data, {
             style: function (feature) {
 
                 if (feature.properties["FLD_ZONE"] == 'X') {
@@ -117,37 +138,50 @@
             }
         }).addTo(floodLayerGroup);
 
-        //console.log(typeof(floodLayer));
-
-
-        var popupTemplate = "<h3>Flood Zone: {FLD_ZONE}</h3><br><h4>100 year? {SFHA_TF}";
+        //popup for geoJson
+        var popupTemplate = "<h3>Flood Zone: {FLD_ZONE}</h3>";
 
         floodLayer.bindPopup(function (e) {
             return L.Util.template(popupTemplate, e.feature.properties)
         });
 
+
+        baseMapControl();
         floodLayerGroup.addTo(map);
-        tiles.addTo(map);
+        $('.loading').hide();
+        createLocationPopup(floodLayer);
+
     }
 
     function createLegend(data) {
 
-        var legColor = '#1c3f6a';
-
-        Qjson = jsonQ(data);
+        var Qjson = jsonQ(data);
         var allZones = Qjson.find('FLD_ZONE');
         var legendContent = allZones.unique();
-        var shap1 = Qjson.find('SHAPE.AREA');
-        var shap1Content = shap1.unique();
+        //var shap1 = Qjson.find('SHAPE.AREA');
+        //var shap1Content = shap1.unique();
         //console.log(shap1Content);
+        var legendColor;
 
         for (var i = 0; i < legendContent.length; i++) {
 
+            if (legendContent[i] == 'A' || legendContent[i] == 'AE') {
+                legendColor = 'red';
+            } else {
+                legendColor = 'blue';
+            }
+
             document.getElementById("addColor").innerHTML +=
-                '<span class="w24 h24" style="background:' + legColor + '"></span>';
+                '<span class="w24 h24" style="opacity:0.4;background-color:' + legendColor + '"></span>';
 
             document.getElementById("addLabel").innerHTML += '<p id="label">' + legendContent[i] + '</p>';
         }
+
+        $('#legend span').hover(function () {
+            $(this).css("border", "2px solid yellow");
+        }, function () {
+            $(this).css("border", "none");
+        });
     }
 
     function geocodeAddress(address) {
@@ -155,9 +189,6 @@
         L.esri.Geocoding.geocode().text(address).run(function (err, results) {
 
             searchLatLng = L.latLng(results.results["0"].latlng.lat, results.results["0"].latlng.lng);
-            //console.log(results);
-            //console.log(bounds);
-
             findNewLocation(searchLatLng);
 
         });
@@ -165,19 +196,31 @@
 
     function findNewLocation(latLng) {
 
-        L.marker(searchLatLng).addTo(map);
-        map.setView(latLng, 13);
+        currentLocation.remove();
+
+        //clear legend info
+        document.getElementById("addColor").innerHTML = '';
+        document.getElementById("addLabel").innerHTML = '';
+
+        currentLocation = L.marker(searchLatLng).addTo(map);
+        map.setView(latLng, 14);
 
 
         floodLayerGroup.clearLayers();
-        bounds = map.getBounds();
+        bounds = map.getBounds().pad(0.8);
         center = map.getCenter();
         queryFloodMap(bounds);
-
-        console.log(bounds)
+        console.log(bounds);
 
     }
 
-    function getColor()
+    function createLocationPopup(floodLayer) {
+
+        var results = leafletPip.pointInLayer(currentLocation.getLatLng(), floodLayer);
+        console.log(results);
+        if (results.length != 0) {
+            currentLocation.bindPopup(results["0"].feature.properties.FLD_ZONE).openPopup();
+        }
+    }
 
 })();
